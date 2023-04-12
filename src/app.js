1aacdf900a2c8f07c2d6acb54bddcb0b3f427d9a
dnomak <me@dnomak.com>
2023-04-12 21:43:01 +0300

    Add index page using pug template engine

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const path = require("path");
+
+const indexRouter = require("./routes/index");
 
 const app = express();
+
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use("/", indexRouter);
 
 module.exports = app;
