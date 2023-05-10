c3aa7026fa57544045efd4bda2dc49a9418e4839
dnomak <me@dnomak.com>
2023-05-11 00:26:16 +0300

    Add startup logs

--- index.ts
+++ index.ts
@@ -7,5 +7,13 @@ dotenv.config()
 const port = process.env.PORT || '3000'
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
+  const { npm_package_name, npm_package_version } = process.env
+  const time = new Date().toTimeString().slice(0, 8)
+  console.log('  ')
+  console.log('  ' + npm_package_name + ' ' + npm_package_version + ' ready at ' + time)
+  console.log('  ')
+  console.log('  ➜  Application: http://localhost:' + port)
+  console.log('  ➜  Inspector: chrome://inspect/#devices')
+  console.log('  ➜  Press CTRL+C to stop')
+  console.log('  ')
 })
