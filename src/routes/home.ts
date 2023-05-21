d07c827c367de8a44bfa713fe933c2ffe49714f6
dnomak <me@dnomak.com>
2023-05-22 00:01:56 +0300

    Add route class to body

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('home', { title: 'node-starter' })
+  res.render('home', { handle: 'home', title: 'node-starter' })
 })
 
 export default router
